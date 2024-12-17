b8b02e06fe682c95295e8be1270d3f7ee862c868
Bastián Loyola Jara <73760899+bastianloyola@users.noreply.github.com>
2024-12-17 18:12:16 -0300

    Fix: comments in neatannarchy.py

--- neatannarchy.py
+++ neatannarchy.py
@@ -10,73 +10,73 @@ def download_neat():
         with open("NEAT", "wb") as file:
             file.write(response.content)
 
-    # Dar permisos de ejecución al archivo
+    # Give execution permissions to the file
     subprocess.run(["chmod", "+x", "NEAT"])
 
 
 
 def write_annarchy(neuron_model, func):
-    # Leer el contenido dinámico de neuronmodel.py
+    # Read the dynamic content of neuronmodel.py
     with open("neuronmodel.py", "r") as neuron_file:
         neuron_model_text = neuron_file.read()
 
-    # Leer el contenido dinámico de fitness.py
+    # Read the dynamic content of fitness.py
     with open("fitness.py", "r") as func_file:
         func_text = func_file.read()
 
-    # Construcción del código base usando listas
+    # Construct the final content of annarchy.py
     lines = [
         "import numpy as np",
         "import random as rd",
         "import scipy.sparse",
         f"from neuronmodel import {neuron_model}",
         "",
-        neuron_model_text.strip(),  # Añadir el contenido de neuronmodel.py
+        neuron_model_text.strip(),  # Add the content of neuronmodel.py
         "",
-        "def snn(n_entrada, n_salida, n, i, matrix, inputWeights, trial):",
+        "def snn(n_inputs, n_outputs, n, i, matrix, inputWeights, trial, genome_id):",
         "    try:",
         "        clear()",
-        "        # Se crea una población de n neuronas",
+        "        # Create a population with n neurons",
         f"        pop = Population(geometry=n, neuron={neuron_model})",
-        "        # Se crea una proyección de las neuronas a sí mismas",
+        "        # Create projection from neurons to itself",
         "        proj = Projection(pre=pop, post=pop, target='exc')",
-        "        # Verificar el tamaño de la matrix",
+        "        # Verify the size of the matrix",
         "        if matrix.size == 0:",
         "            raise ValueError('matrix is empty')",
-        "        # lil_matrix scipy nxn con los valores de la matrix",
+        "        # lil_matrix scipy nxn with values from matrix",
         "        lil_matrix = scipy.sparse.lil_matrix((int(n), int(n)))",
         "        n_rows = matrix.shape[0]",
         "        n_cols = matrix.shape[1]",
         "        lil_matrix[:n_rows, :n_cols] = matrix",
         "        proj.connect_from_sparse(lil_matrix)",
-        "        nombre = 'annarchy/annarchy-' + str(int(trial)) + '/annarchy-' + str(int(i))",
-        "        compile(directory=nombre, clean=False, silent=True)",
+        "        name = 'annarchy/annarchy-' + str(int(trial)) + '/annarchy-' + str(int(i))",
+        "        compile(directory=name, clean=False, silent=True)",
         "        M = Monitor(pop, ['spike', 'v'])",
         "        input_index = []",
         "        output_index = []",
-        "        n_entrada = int(n_entrada)",
-        "        n_salida = int(n_salida)",
-        "        for i in range(n_entrada):",
+        "        n_inputs = int(n_inputs)",
+        "        n_outputs = int(n_outputs)",
+        "        for i in range(n_outputs):",
         "            input_index.append(i)",
-        "        for i in range(n_entrada, n_salida + n_entrada):",
+        "        for i in range(n_inputs, n_outputs + n_inputs):",
         "            output_index.append(i)",
-        "        # Verificar el tamaño de inputWeights",
+        "        # Verify size of inputWeights",
         "        if inputWeights.size == 0:",
         "            raise ValueError('inputWeights is empty')",
         "",
         f"        fit = {func}(pop, M, input_index, output_index, inputWeights)",
         "        return fit",
         "    except Exception as e:",
-        "        # Capturar y manejar excepciones",
+        "        # Capture the error and print it",
         "        print('Error en annarchy:', e)",
         "",
-        func_text.strip()  # Añadir el contenido de fitness.py
+        func_text.strip()  # Add the content of fitness.py
     ]
 
-    # Escribir las líneas en el archivo final
+    # Write the content to annarchy.py
     with open("annarchy.py", "w", newline='\n') as f:
         f.write("\n".join(lines))
-        f.write("\n")  # Asegurar una línea final
+        f.write("\n") 
 
 
 def get_function():
@@ -118,7 +118,8 @@ def runNEAT(trial, func, neuron_model, procesos, evolutions, population):
     process.wait()
 
     if process.returncode != 0:
-        print("Error durante la ejecución:", error)
+        print("Error running NEAT:", error)
     else:
         fitness = float(output.strip().split("\n")[-1])
         return fitness
+    
